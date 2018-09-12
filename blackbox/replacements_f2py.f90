a907b7269834d750094f2b7478fb2e0efd281d8a
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2018-09-12 19:42:01 +0200

    refactoring cleanup, all regression tests passed

--- blackbox/replacements_f2py.f90
+++ blackbox/replacements_f2py.f90
@@ -9,31 +9,27 @@ SUBROUTINE f2py_spread(rslt, points, n, d)
 
     DOUBLE PRECISION, INTENT(OUT)   :: rslt
 
+    INTEGER, INTENT(IN)             :: d
+    INTEGER, INTENT(IN)             :: n
 
-    INTEGER, INTENT(IN)             :: n, d
     DOUBLE PRECISION, INTENT(IN)    :: points(n, d)
 
+    INTEGER                         :: j
+    INTEGER                         :: i
 
-    INTEGER                         ::i, j
-
-    !TODO: Review best practices
 !-------------------------------------------------------------------------------
 ! Algorithm
 !-------------------------------------------------------------------------------
-
     rslt = 0.0
     DO i = 1, n
-        DO j = 1, n
-            IF (i .GT. j) THEN
-                rslt = rslt + 1.0 / NORM2(points(i, :) - points(j, :))
-            END IF
+        DO j = 1, i - 1
+            rslt = rslt + 1.0 / NORM2(points(i, :) - points(j, :))
         END DO
     END DO
 
 END SUBROUTINE
 !*******************************************************************************
 !*******************************************************************************
-
 SUBROUTINE f2py_get_capital_phi(rslt, points, T, n, d)
 
     !/* setup                   */
@@ -41,23 +37,23 @@ SUBROUTINE f2py_get_capital_phi(rslt, points, T, n, d)
     IMPLICIT NONE
 
     DOUBLE PRECISION, INTENT(OUT)   :: rslt(n, n)
-    DOUBLE PRECISION, INTENT(IN)               :: T(d, d)
-    INTEGER, INTENT(IN)             :: d
 
-    INTEGER, INTENT(IN)             :: n
     DOUBLE PRECISION, INTENT(IN)    :: points(n, d)
+    DOUBLE PRECISION, INTENT(IN)    :: T(d, d)
 
+    INTEGER, INTENT(IN)             :: d
+    INTEGER, INTENT(IN)             :: n
 
-    INTEGER                         ::i, j
+    DOUBLE PRECISION                :: substract(d)
 
-    DOUBLE PRECISION :: substract(d)
-    !TODO: Review best practices
+    INTEGER                         :: i
+    INTEGER                         :: j
+
 !-------------------------------------------------------------------------------
 ! Algorithm
 !-------------------------------------------------------------------------------
 
     rslt = -99.0
-
     DO i = 1, n
         DO j = 1, n
             substract(:) = points(i, :) - points(j, :)
@@ -66,10 +62,6 @@ SUBROUTINE f2py_get_capital_phi(rslt, points, T, n, d)
     END DO
 
 END SUBROUTINE
-
-!*******************************************************************************
-!*******************************************************************************
-
 !*******************************************************************************
 !*******************************************************************************
 SUBROUTINE f2py_fit_full(rslt, lam, b, a, T, points, x)
@@ -78,27 +70,33 @@ SUBROUTINE f2py_fit_full(rslt, lam, b, a, T, points, x)
 
     IMPLICIT NONE
 
-    DOUBLE PRECISION, INTENT(OUT):: rslt
+    DOUBLE PRECISION, INTENT(OUT)   :: rslt
 
-    DOUBLE PRECISION, INTENT(IN)          :: lam(:), b(:), a(:), T(:, :), points(:, :), x(:, :)
+    DOUBLE PRECISION, INTENT(IN)    :: points(:, :)
+    DOUBLE PRECISION, INTENT(IN)    :: x(:, :)
+    DOUBLE PRECISION, INTENT(IN)    :: T(:, :)
+    DOUBLE PRECISION, INTENT(IN)    :: lam(:)
+    DOUBLE PRECISION, INTENT(IN)    :: b(:)
+    DOUBLE PRECISION, INTENT(IN)    :: a(:)
 
+    DOUBLE PRECISION                :: substr(SIZE(T, 1))
+    DOUBLE PRECISION                :: incr(1)
 
-    DOUBLE PRECISION :: incr(1)
+    INTEGER                         :: i
 
-    DOUBLE PRECISION, ALLOCATABLE   :: substr(:)
-    INTEGER :: n, i, d
 !-------------------------------------------------------------------------------
 ! Algorithm
 !-------------------------------------------------------------------------------
-    n = SIZE(lam)
+
     incr = MATMUL(b, x) + a
-    d = SIZE(T, 1)
-    ALLOCATE(substr(d))
-rslt = 0.0
-    DO i = 1, n
+
+    rslt = 0.0
+    DO i = 1, SIZE(lam)
         substr = x(:, 1) - points(i, :)
         rslt = rslt + lam(i) * NORM2(MATMUL(T, substr)) ** 3
     END DO
     rslt = rslt + incr(1)
 
 END SUBROUTINE
+!*******************************************************************************
+!*******************************************************************************
